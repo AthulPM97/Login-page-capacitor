11d2affd6516d8757c4b6b0e359dc66006ee276d
athulpm <97athulpm@gmail.com>
2023-02-24 23:32:36 +0530

    Sign up screen with Ion components

--- src/App.js
+++ src/App.js
@@ -2,23 +2,32 @@ import { Link, Route, Switch } from "react-router-dom";
 import Dashboard from "./routes/Dashboard";
 import Login from "./routes/Login";
 
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+import "./theme/variables.css"
+
+import { setupIonicReact } from "@ionic/react";
+setupIonicReact();
+
 function App() {
   return (
-    <div style={{margin:'1rem',paddingTop: 'env(safe-area-inset-top)'}}>
-    <Switch>
-      <Route path="/" exact>
-        <Login/>
-      </Route>
-      <Route path="/app/dashboard" >
-        <Dashboard/>
-      </Route>
-      <Route path="*">
-        <main>
-          <p>There is nothing here</p>
-          <Link to="/">Back home</Link>
-        </main>
-      </Route>
-    </Switch>
+    <div style={{ margin: "0 1rem 0 1rem", paddingTop: "env(safe-area-inset-top)" }}>
+      <h1 style={{textAlign:'center'}}>Login page</h1>
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route path="/app/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="*">
+          <main>
+            <p>There is nothing here</p>
+            <Link to="/">Back home</Link>
+          </main>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,29 +1,59 @@
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonInput,
+  IonItem,
+  IonLabel,
+} from "@ionic/react";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const Login = () => {
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        setLoading(true);
-        console.log("Submit");
-        setTimeout(() => {
-            setLoading(false);
-            history.push('/app/dashboard');
-        }, 1500);
-    }
-    return(
-        <main style={{textAlign:'center'}}>
-        <h1>Sign in</h1>
+  const emailChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+  const passwordChangeHandler = (e) => {
+    console.log(e.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      history.push("/app/dashboard");
+    }, 1500);
+  };
+  return (
+    <IonCard>
+      <IonCardContent>
         <form onSubmit={submitHandler}>
-            {!loading && <button type="submit">Sign in</button>}
-            {loading && <p>Loading...</p>}
+          <IonItem>
+            <IonLabel position="floating">Email</IonLabel>
+            <IonInput type="email" onIonChange={emailChangeHandler} />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Password</IonLabel>
+            <IonInput type="password" onIonChange={passwordChangeHandler} />
+          </IonItem>
+          <br />
+          <div className="ion-margin-top">
+            {!loading && (
+              <IonButton type="submit" color="secondary" expand="full">
+                Login
+              </IonButton>
+            )}
+            {loading && <p style={{ textAlign: "center" }}>Loading...</p>}
+          </div>
         </form>
-        </main>
-    )
-}
+      </IonCardContent>
+    </IonCard>
+  );
+};
 
 export default Login;
